b6d5b7cbfd418a8ccf07836f6dd1cc232f3f9571
extazystas <2153311+Extazystas@users.noreply.github.com>
2024-01-07 15:57:53 +0200

    Add CurrencyConverter

---
+++ app/services/currency_converter.rb
@@ -0,0 +1,15 @@
+class CurrencyConverter
+  RATES = {
+    'CHF' => 1.0,
+    'USD' => 1.18,
+    'EUR' => 1.07
+  }.freeze
+
+  def self.convert(amount, from, to)
+    return amount if (from == to)
+
+    base_amount = amount / RATES[from]
+
+    (base_amount * RATES[to]).floor(2)
+  end
+end
